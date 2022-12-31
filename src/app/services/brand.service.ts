33afee24b7d26bfde8750afe8fb1a47a16596d0a
aktepeayse96 <aktepeayse96@gmail.com>
2022-12-31 20:35:02 +0300

    brand ilk

--- src/app/services/brand.service.ts
+++ src/app/services/brand.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Observable } from 'rxjs';
+import { Brand } from '../models/brand'; 
 @Injectable({
   providedIn: 'root'
 })
 export class BrandService {
 
-  constructor() { }
+  apiUrl = 'https://localhost:7175/api/brands/getall';
+
+  constructor(private httpClient: HttpClient) { }
+  
+  getBrands():Observable<ListResponseModel<Brand>> {
+    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl);
+  }
+
 }
