768359e86dc440d0502729c10ef483757405ab07
aktepeayse96 <aktepeayse96@gmail.com>
2023-01-01 14:18:50 +0300

    color başlangıç

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Observable } from 'rxjs';
+import { Color } from '../models/color'; 
 @Injectable({
   providedIn: 'root'
 })
 export class ColorService {
 
-  constructor() { }
+  apiUrl = 'https://localhost:7175/api/colors/getbydetail';
+  constructor(private httpClient: HttpClient) { }
+
+  getColors():Observable<ListResponseModel<Color>> {
+    return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl);
+  }
 }
