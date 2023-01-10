ee21eaa73360cc3cf8cb15696ddadc3ab53a16c5
aktepeayse96 <aktepeayse96@gmail.com>
2023-01-10 16:45:59 +0300

    10 ocak 1

--- src/app/models/customer.ts
+++ src/app/models/customer.ts
@@ -1,4 +1,5 @@
 export interface Customer{
     //Id:number;
+    //UserId:number;
     companyName:string;
 }
--- src/app/services/rental.service.ts
+++ src/app/services/rental.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Observable } from 'rxjs';
+import { Rental } from '../models/rental';
 @Injectable({
   providedIn: 'root'
 })
 export class RentalService {
 
-  constructor() { }
+  apiUrl = 'https://localhost:7175/api/rentals/getwithdetail';
+  constructor(private httpClient: HttpClient) { }
+
+  getRentals():Observable<ListResponseModel<Rental>> {
+    return this.httpClient.get<ListResponseModel<Rental>>(this.apiUrl);
+  }
 }
